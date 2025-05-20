4224ca7e66f6689a4421e4c5cba60fbcc70f14ca
Michael Moore <moore.m@me.com>
2025-05-20 12:13:34 -0400

    Updated StockError

--- Stonks/Utilities/StockError.swift
+++ Stonks/Utilities/StockError.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 // Errors that can occur when fetching stocks
-enum StockError: Error, LocalizedError {
+enum StockError: Error, LocalizedError, Equatable {
     case failedToLoadData
     case invalidResponse
     case networkUnavailable
@@ -55,6 +55,22 @@ enum StockError: Error, LocalizedError {
             return "This appears to be a data format issue. Please contact support if the problem persists."
         case .fileNotFound:
             return "Please reinstall the app if this issue continues."
+        }
+    }
+    
+    // Custom equality implementation for cases with associated values
+    static func == (lhs: StockError, rhs: StockError) -> Bool {
+        switch (lhs, rhs) {
+        case (.failedToLoadData, .failedToLoadData),
+             (.invalidResponse, .invalidResponse),
+             (.networkUnavailable, .networkUnavailable):
+            return true
+        case (.decodingFailed, .decodingFailed):
+            return true // Compare by case type only for DecodingError
+        case (.fileNotFound(let lhsFile), .fileNotFound(let rhsFile)):
+            return lhsFile == rhsFile
+        default:
+            return false
         }
     }
 }
